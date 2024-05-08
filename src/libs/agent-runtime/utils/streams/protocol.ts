22f8c6f91cc53dc499ef2d5aaa6d399ce658b77f
arvinxx <arvinx@foxmail.com>
2024-05-09 02:12:39 +0800

    ✨ feat: support mistral tool calling

--- src/libs/agent-runtime/utils/streams/protocol.ts
+++ src/libs/agent-runtime/utils/streams/protocol.ts
@@ -15,7 +15,18 @@ export const transformOpenAIStream = (chunk: OpenAI.ChatCompletionChunk): Stream
   }
 
   if (item.delta?.tool_calls) {
-    return { data: item.delta.tool_calls, id: chunk.id, type: 'tool_calls' };
+    return {
+      data: item.delta.tool_calls.map((value, index) => ({
+        ...value,
+
+        // mistral's tool calling don't have index and function field, it's data like:
+        // [{"id":"xbhnmTtY7","function":{"name":"lobe-image-designer____text2image____builtin","arguments":"{\"prompts\": [\"A photo of a small, fluffy dog with a playful expression and wagging tail.\", \"A watercolor painting of a small, energetic dog with a glossy coat and bright eyes.\", \"A vector illustration of a small, adorable dog with a short snout and perky ears.\", \"A drawing of a small, scruffy dog with a mischievous grin and a wagging tail.\"], \"quality\": \"standard\", \"seeds\": [123456, 654321, 111222, 333444], \"size\": \"1024x1024\", \"style\": \"vivid\"}"}}]
+        index: typeof value.index !== 'undefined' ? value.index : index,
+        type: value.type || 'function',
+      })),
+      id: chunk.id,
+      type: 'tool_calls',
+    };
   }
 
   // 给定结束原因
