bc3e817854603a8796cbd90ea5c6b679037927a6
arvinxx <arvinx@foxmail.com>
2024-05-09 02:48:59 +0800

    🎨 feat: improve minimax tool calling stability

--- src/libs/agent-runtime/utils/streams/protocol.ts
+++ src/libs/agent-runtime/utils/streams/protocol.ts
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 
-interface StreamProtocolChunk {
+export interface StreamProtocolChunk {
   data: any;
   id: string;
   type: 'text' | 'tool_calls' | 'data' | 'stop';
@@ -21,6 +21,11 @@ export const transformOpenAIStream = (chunk: OpenAI.ChatCompletionChunk): Stream
 
         // mistral's tool calling don't have index and function field, it's data like:
         // [{"id":"xbhnmTtY7","function":{"name":"lobe-image-designer____text2image____builtin","arguments":"{\"prompts\": [\"A photo of a small, fluffy dog with a playful expression and wagging tail.\", \"A watercolor painting of a small, energetic dog with a glossy coat and bright eyes.\", \"A vector illustration of a small, adorable dog with a short snout and perky ears.\", \"A drawing of a small, scruffy dog with a mischievous grin and a wagging tail.\"], \"quality\": \"standard\", \"seeds\": [123456, 654321, 111222, 333444], \"size\": \"1024x1024\", \"style\": \"vivid\"}"}}]
+
+        // minimax's tool calling don't have index field, it's data like:
+        // [{"id":"call_function_4752059746","type":"function","function":{"name":"lobe-image-designer____text2image____builtin","arguments":"{\"prompts\": [\"一个流浪的地球，背景是浩瀚"}}]
+
+        // so we need to add these default values
         index: typeof value.index !== 'undefined' ? value.index : index,
         type: value.type || 'function',
       })),
