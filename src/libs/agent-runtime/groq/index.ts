0a7f0fdb7e71359e1fe4188442d59444266896a1
Arvin Xu <arvinx@foxmail.com>
2024-05-08 04:31:40 +0000

    ✨ feat: support groq tool callings

--- src/libs/agent-runtime/groq/index.ts
+++ src/libs/agent-runtime/groq/index.ts
@@ -4,6 +4,15 @@ import { LobeOpenAICompatibleFactory } from '../utils/openaiCompatibleFactory';
 
 export const LobeGroq = LobeOpenAICompatibleFactory({
   baseURL: 'https://api.groq.com/openai/v1',
+  chatCompletion: {
+    handlePayload: (payload) => {
+      return {
+        ...payload,
+        // disable stream for tools due to groq dont support
+        stream: !payload.tools,
+      } as any;
+    },
+  },
   debug: {
     chatCompletion: () => process.env.DEBUG_GROQ_CHAT_COMPLETION === '1',
   },
